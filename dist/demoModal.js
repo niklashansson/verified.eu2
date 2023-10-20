99542294ad7732052a84e2da70086f179989302c
niklashansson <niklas@bloomweb.co>
2023-10-20 17:10:28 +0200

    Made dist folder visible in .gitignore

---
+++ dist/demoModal.js
@@ -0,0 +1,35 @@
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/demoModal.ts
+  window.Webflow = window.Webflow || [];
+  window.Webflow.push(async () => {
+    const buttons = document.querySelectorAll('a[href="#book-demo"]');
+    const modal = document.querySelector('[verified-demo-element="modal"]');
+    if (!buttons.length || !modal)
+      return;
+    const closeButtons = modal.querySelectorAll('[verified-demo-element="close-button"]');
+    if (!closeButtons.length)
+      return;
+    buttons.forEach((btn) => {
+      btn.setAttribute("href", "");
+      btn.addEventListener("click", () => {
+        openModal();
+      });
+    });
+    function openModal() {
+      closeButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          closeModal();
+        });
+      });
+      modal.classList.add("is-active");
+    }
+    function closeModal() {
+      modal.classList.remove("is-active");
+    }
+  });
+})();
+//# sourceMappingURL=demoModal.js.map
